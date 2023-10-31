a6b999e20e1bbd976e7db0eeb5bd35abeef09937
Wilson-Wu1 <wilsonwuuu@gmail.com>
2023-10-31 00:29:46 -0700

    Added logic for URL and transaction type

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,47 +1,73 @@
-import type { OnTransactionHandler } from '@metamask/snaps-types';
+import type { OnTransactionHandler, OnRpcRequestHandler } from '@metamask/snaps-types';
 import { heading, panel, text, copyable, divider } from '@metamask/snaps-ui';
 import { hasProperty } from '@metamask/utils';
-
 import { getHashDitResponse } from "./utils/utils";
 import { SUPPORTED_CHAINS } from "./utils/chains";
+
 
 // Handle outgoing transactions.
 export const onTransaction: OnTransactionHandler = async ({ transaction, chainId, transactionOrigin }) => {
   //const response = await getHashDitResponse(transaction);
   //console.log(response);
 
-  // Check if the current chain is supported by this Snap. If not supported, display "not supported" text.
-  const explorerURL = SUPPORTED_CHAINS[chainId]?.url;
-  if(explorerURL === undefined){
-    return{
-      content: panel([
-        heading('HashDit Security Insights'),
-        text("HashDit Security Insights is not supported on this chain."),
-        text("Currently we support **Ethereum Mainnet**, **Sepolia Testnet**, **BSC Mainnet**, and **BSC Testnet**")
-        ]),
+
+  /* 
+    Transaction is native token transfer, therefore only check if current chain is supported.
+    The key `data` in object `transaction` only exists in smart contract interactions and not native transfers.
+    We can use this to determine the type of transaction (native transfer or contract interaction).
+  */ 
+  if (!transaction.hasOwnProperty('data')) {
+    // Check if the current chain is supported by this Snap. If not supported, display "not supported" text.
+    const explorerURL = SUPPORTED_CHAINS[chainId]?.url;
+    if(explorerURL === undefined){
+      return{
+        content: panel([
+          heading('HashDit Security Insights'),
+          text("HashDit Security Insights is not supported on this chain."),
+          text("Currently we support **Ethereum Mainnet**, **Sepolia Testnet**, **BSC Mainnet**, and **BSC Testnet**")
+        ])
+      }
+    }
+    // Otherwise, display token transfer insights
+    else{
+      return {
+        content: panel([
+          text(`${transaction.data}`),
+          heading('HashDit Security Insights'),
+          text(
+            `You are transfering **${transaction.value}** to **${transaction.to}**`
+          ),
+          divider(),
+          heading('HashDit Security Response'),
+          text(
+            `HashDit Response: `,
+            ),
+    
+          divider(),
+          heading(
+            `View Destination Address On Explorer`
+          ),
+          copyable(`${explorerURL}${transaction.to}`),
+        ])
+      }
     }
   }
 
-  
-
+  // Transaction is an interaction with a smart contract because key `data` was found in object `transaction`
   return {
     content: panel([
-      text(`${transaction.data}`),
       heading('HashDit Security Insights'),
       text(
-        `As set up, you are transfering to **${transaction.to}**`
+        `You are interacting with contract **${transaction.to}**`
       ),
       divider(),
       heading('HashDit Security Response'),
       text(
         `HashDit Response: `,
         ),
-
       divider(),
-      heading(
-        `View Destination Address On Explorer`
-      ),
-      copyable(`${explorerURL}${transaction.to}`),
+      // Todo: Call HashDit api here to determine if a url is safe
+      text(`The url **${transactionOrigin}** is safe/malicious`)
       ]),
       
     };
