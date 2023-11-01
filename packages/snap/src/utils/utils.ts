ac3db57e42772a3e834a3a77308c08d238ff161b
patryk-loda-binance <patryk.loda@binance.com>
2023-11-01 14:52:24 +0000

    HashDit api business name and logic update

--- packages/snap/src/utils/utils.ts
+++ packages/snap/src/utils/utils.ts
@@ -49,44 +49,86 @@ export const isEthereumAddress = (address: string) => {
 };
 
 
-async function getHashDitResponse(transaction: any) {
-  const body = {
-    "address":transaction.to,
-    "chain_id":"56"};
+export async function getHashDitResponse(transaction: any, transactionUrl: any, chainId: string, businessName: string) {
+  console.log("getHashDitResponse");
+  console.log(transaction);
+  console.log(transactionUrl, chainId, businessName);
+
+  // formatting chainid to match api formatting
+  let chain: string;
+  switch (chainId) {
+      case "eip155:1":
+        chain = "0x1";
+        break;
+      case "eip155:38":
+        chain = "0x56";
+        break;
+      default:
+        chain = "0x1"; // only to stop errors, need to find good default
+  }
+
+  let postBody: any = {};
+  if (businessName == "hashdit_snap_tx_api_url_detection") {
+    postBody.url = transactionUrl;
+
+  } else if (businessName == "address_screening") {
+    postBody.address = transaction.to;
+    postBody.chain_id = chain;
   
+  } else if (businessName == "hashdit_snap_tx_api_transaction_request") {
+    postBody.address = transaction.to;
+    postBody.chain_id = chain;
+    postBody.trace_id = "5c978e09-1508-4ab0-8550-4d3e6640b9d4"; // random string e.g. "5c978e09-1508-4ab0-8550-4d3e6640b9c3", need to be generated
+    postBody.transaction = transaction;
+    postBody.url = transactionUrl;
+
+  } else if (businessName == "hashdit_snap_tx_api_signature_request") {
+    postBody.address = transaction.to;
+    postBody.chain_id = chain;
+    postBody.message = "0xdeadbeef"; // should be signature message
+    postBody.method = "eth_sign";
+    postBody.trace_id = "5c978e09-1508-4ab0-8550-4d3e6640b9d4"; // random string e.g. "5c978e09-1508-4ab0-8550-4d3e6640b9c3", needs to be generated
+    postBody.url = transactionUrl;
+  }
+  console.log("postbody: ", postBody);
   // Extension code will be opensource - Need to hide these keys
-  const appId = '42b7d48e81754984b624';
-  const appSecret = '03909eb04c894bd29a79f9e1127847c6';
+  const appId = 'a3d194daa5b64414bbaa';
+  const appSecret = 'b9a0ce86159b4eb4ab94bbb80503139d';
   const timestamp = new Date().getTime();
   const nonce = uuidv4().replace(/-/g, '');
 
-  const bodyString = JSON.stringify(body);
-  const dataToSign = `${appId};${timestamp};${nonce};POST;/security-api/public/app/v1/detect;${JSON.stringify(bodyString)}`;
+  const dataToSign = `${appId};${timestamp};${nonce};POST;/security-api/public/app/v1/detect;${JSON.stringify(postBody)}`;
   const hash = CryptoJS.HmacSHA256(dataToSign, appSecret);
   const signature = CryptoJS.enc.Hex.stringify(hash);
 
-  const header = {
-    "Content-Type": "application/json;charset=UTF-8",
-    "X-Signature-appid": appId,
-    "X-Signature-timestamp": timestamp,
-    "X-Signature-nonce": nonce,
-    "X-Signature-signature": signature
-  };
+  const url = new URL('https://api.hashdit.io/security-api/public/app/v1/detect');
+  url.searchParams.append("business", businessName);
 
-  const response = await fetch('https://api.hashdit.io/security-api/public/app/v1/detect', {
+  const response = await fetch(url, {
     method: 'POST',
-    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
     headers: {
       "Content-Type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin":"*", 
       "X-Signature-appid": appId,
       "X-Signature-timestamp": timestamp.toString(),
       "X-Signature-nonce": nonce,
       "X-Signature-signature": signature
     },
-    body: JSON.stringify(body),
-    redirect: "follow"
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(postBody),
   });
-  return await response.json();
+
+  const resp = await response.json();
+  console.log("response: ", resp);
+
+  if (resp.status == "OK" && resp.data) {
+    return resp.data;
+  } else {
+    throw Error("Fetch api error: " + resp.errorData);
+  }
 }
 
 
